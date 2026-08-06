c4caa4801a04b896e6a5a737dcc7df3e2b94b782
Pankaj Dwivedi <pankajdwivedi077@gmail.com>
2026-08-06 17:26:05 +0530

    crud op in postgresql from npm pg

--- step 4/1.postgresql/src/conceps/basic.queries.js
+++ step 4/1.postgresql/src/conceps/basic.queries.js
@@ -17,4 +17,74 @@ async function createUserTable(){
     }
 }
 
-module.exports = { createUserTable };
+// prevent sql injection by ($)
+async function insertUser(username, email){
+  const insertUserQuery = `
+  INSERT INTO users (username, email)
+  VALUES ($1, $2)
+  RETURNING *
+   `
+  try{
+   const res = await db.query(insertUserQuery, [username, email])
+   console.log("user inserted successfully", res.rows[0]);
+   return res.rows[0];
+  }catch(e){
+      console.error("error while creating user table ", e)
+  }
+ 
+}
+ 
+async function getUsers(){
+   const getAllUsers = `SELECT * FROM users`;
+   try{
+    const res = await db.query(getAllUsers)
+    console.log('fetched all users')
+    return res.rows;
+   }catch(e){
+        console.error("error while creating user table ", e)
+   }
+}
+
+async function updateUserEmail(username, email){
+
+  const updateQuery = `
+   UPDATE users
+   SET email = $2
+   WHERE username = $1
+   RETURNING *
+  `
+  
+   try{
+     const res = await db.query(updateQuery, [username, email]);
+     if(res.rows.length > 0){
+      console.log("user updated successfully")
+      return res.rows[0]
+     }else{
+      console.log("no user found")
+      return null;
+     }
+   
+   }catch(e){
+       console.error("error while creating user table ", e)
+   }
+}
+
+async function deleteUser(id){
+  const deletedQuery = `
+   DELETE FROM users
+   WHERE id = $1
+   RETURNING *
+  `
+  try{
+      const res = await db.query(deleteUser, [id])
+      if(res.rows.length > 0){
+        console.log("user deleted successfully")
+      }else{
+        console.log("no user found")
+      }
+  }catch(e){
+    console.error("error while creating user table ", e)
+  }
+}
+
+module.exports = { createUserTable, insertUser, getUsers, updateUserEmail, deleteUser };
--- step 4/1.postgresql/src/main.js
+++ step 4/1.postgresql/src/main.js
@@ -1,9 +1,24 @@
-const { createUserTable } = require("./conceps/basic.queries");
+const { createUserTable, insertUser, getUsers, updateUserEmail, deleteUser } = require("./conceps/basic.queries");
 
 // test basic query
 async function testBasicQueries(){
     try{
-      await createUserTable()
+    //  await createUserTable()
+
+    // await insertUser("Bell", "Bell@gmail.com")
+
+    // console.log("all users")
+    // const allUsers = await getUsers();
+    // console.log(allUsers);
+
+    // const updated = await updateUserEmail("Bell", "Chan@gmail.com");
+    // console.log(updated)
+
+    // const deleted = await deleteUser(1)
+    // console.log(deleted)
+
+    
+
     }catch(e){
         console.error("Error ", e)
     }
