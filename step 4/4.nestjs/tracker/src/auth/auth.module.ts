cdb94d5624190a31858167f4156eb9e3fd7a1af4
Pankaj Dwivedi <pankajdwivedi077@gmail.com>
2026-08-14 15:39:49 +0530

    jwt

--- step 4/4.nestjs/tracker/src/auth/auth.module.ts
+++ step 4/4.nestjs/tracker/src/auth/auth.module.ts
@@ -2,12 +2,31 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
-    PrismaModule
+    PrismaModule,
+    PassportModule,
+    // JwtModule.register({
+    //   secret: process.env.JWT_SECRET,
+    //   signOptions: { expiresIn: "1h" }
+    // })
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET') || 'JWT_SECRET',
+        signOptions: { expiresIn: '1h' },
+      }),
+    }),
+    ConfigModule
   ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, JwtStrategy],
+  exports: [JwtStrategy, PassportModule]
 })
 export class AuthModule {}
--- step 4/4.nestjs/tracker/src/auth/auth.service.ts
+++ step 4/4.nestjs/tracker/src/auth/auth.service.ts
@@ -1,12 +1,14 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { RegisterDto } from './dto/register.dto';
 import * as bcrypt from "bcrypt";
+import { LoginDto } from './dto/login.dto';
+import { JwtService } from "@nestjs/jwt"
 
 @Injectable()
 export class AuthService {
 
-    constructor(private prisma: PrismaService){}
+    constructor(private prisma: PrismaService, private jwtService: JwtService){}
 
     // handle the new user registration
     async register(registerDto: RegisterDto){
@@ -39,6 +41,36 @@ export class AuthService {
 
         const { password: _, ...result } = newlyCreatedUser
         return result
+    }
+
+    async logic(logicDto: LoginDto){
+
+       const { email, password } = logicDto
+
+       // find the currect user by email as email is uniwue property
+       const user = await this.prisma.user.findUnique({
+        where: {
+            email
+        }
+       })
+        
+       if(!user){
+        throw new UnauthorizedException("Invalid credentials!")
+       }
+
+       // verify the password
+       const isPasswordValid = await bcrypt.compare(password, user.password);
+
+       if(!isPasswordValid){
+        throw new UnauthorizedException("Invalid credentials!")
+       }
+
+       const token = this.jwtService.sign({userId: user.id})
+
+       const { password: _, ...result } = user;
+
+       return { ...result, token }
+
     }
 
 }
