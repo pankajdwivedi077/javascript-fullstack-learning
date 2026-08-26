1d638653beaedbccefd3cd342e20368d75d0f85e
Pankaj Dwivedi <pankajdwivedi077@gmail.com>
2026-08-26 11:41:40 +0530

    register user endpoint

--- step 5/step2/concepts/src/auth/auth.module.ts
+++ step 5/step2/concepts/src/auth/auth.module.ts
@@ -3,12 +3,22 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User])
+    TypeOrmModule.forFeature([User]),
+
+    // passport module
+    PassportModule,
+
+    //configure jwt
+    JwtModule.register({})
+
   ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
+  exports: [AuthService]
 })
 export class AuthModule {}
